d1e2ecb1cd84a9cbf365672b053a80fea6fc794b
ismvin.dev <lqv15112007@gmail.com>
2026-03-18 13:55:39 +0700

    Add normal_equation to model imports in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from .data_loader import preprocess_data
-from .model import gradient_decent, predict_price, adam_optimizer
+from .model import gradient_decent, predict_price, adam_optimizer, normal_equation
 from .vizualization import save_learning_curve
